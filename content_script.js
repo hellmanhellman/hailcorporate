89c04ef6469ee5b382a9d063c703340c9f7edf79
Jonas Hellman <jonashellman@gmail.com>
2017-04-01 22:28:07 +0200

    Enable tagging of posts/users you think are ads/shills

--- content_script.js
+++ content_script.js
@@ -1,9 +1,63 @@
+// minimum number of votes to classify something as suspected
+var cutoff = 4;
+
+var current_user = $('.user a:first').text();
+
+$("body").on("click", ".suspectpost", function(e) {
+    e.preventDefault();
+    var packet = {
+      accuser_username: current_user,
+      suspect_post: $(this).attr('value')
+    };
+    $(this).text('thanks!');
+    $.ajax({
+      type: 'POST',
+      url: 'https://yogatime.herokuapp.com/api/hail_suspect',
+      dataType: 'json',
+      data: packet,
+      success: function() {
+        $(this).text('');
+      }
+    });
+});
+
+$("body").on("click", ".suspectuser", function(e) {
+    e.preventDefault();
+    var packet = {
+      accuser_username: current_user,
+      suspect_username: $(this).attr('value')
+    };
+    $(this).text('unmark shill');
+    $.ajax({
+      type: 'POST',
+      url: 'https://yogatime.herokuapp.com/api/hail_suspect',
+      dataType: 'json',
+      data: packet
+    });
+});
+
+$("body").on("click", ".unsuspectuser", function(e) {
+    e.preventDefault();
+    var packet = {
+      accuser_username: current_user,
+      unsuspect_username: $(this).attr('value')
+    };
+    $(this).text('mark shill');
+    $.ajax({
+      type: 'POST',
+      url: 'https://yogatime.herokuapp.com/api/hail_unsuspect',
+      dataType: 'json',
+      data: packet
+    });
+});
 
 
 // A dictionary which will contain as keys the urls of
 // the urls that are on r/hailcorporate, with the values
 // being the associated upvotes there.
 var hail_url_upvotes = {};
+var api_url_suspicion = {};
+var api_username_suspicion = {};
 
 // Returns gray from 220 to 140 based on upvotes on r/hailcorporate.
 // More upvotes -> More probable it's an ad -> blacker color.
@@ -13,29 +67,98 @@ function colorFromVotes(votes) {
   return "rgb("+v+","+v+","+v+")";
 }
 
+// Same as above but broader, for the api (which will generate more votes per post im supposing).
+// 435 votes makes this the darkest it can be (100/255).
+function colorFromSuspicion(count) {
+  var v = (245 - Math.min(Math.floor(count/3), 145)).toString();
+  return "rgb("+v+","+v+","+v+")";
+}
+
+function defined(v) {
+  return (v !== undefined) && (v !== null) && (v !== "");
+}
 // Modifies the current pages posts (called on page load and after never-ending-reddit update)
 function modifyPosts() {
-  // For each post (thing) ...
+
+
+  // Modify posts.
   $( ".thing" ).each(function(i, thing ) {
 
-            // Grab the post comment section url...
-            var url = $(thing).find(".entry .flat-list li.first a").attr('href')
-            if(typeof url !== "undefined" &&
-               typeof hail_url_upvotes[url.toLowerCase()] !== "undefined" ) {
-              var votes = hail_url_upvotes[url.toLowerCase()][0];
-              var title = $(thing).find(".entry p.title");
+            // Grab the post comment section url and username...
+            var url = $(thing).find(".entry ul.flat-list li.first a").attr('href');
+            var username = $(thing).find(".entry .tagline a.author").text();
 
-              // And if it matches one on r/hailcorporate,
-              // Prepend "(ad)" to the title and set the post background accordingly.
-              // (more upvoted on r/hailcorporate -> darker background)
-              title.prepend('<a href="'+hail_url_upvotes[url.toLowerCase()][1]+'">(ad) </a>');
-              $(thing).animate({backgroundColor:colorFromVotes(votes)},1000);
+            var title = $(thing).find(".entry p.title");
+
+
+            if(defined(url)) {
+
+              url = url.toLowerCase();
+              var flat_list = $(thing).find(".entry ul.flat-list");
+              if(defined(api_url_suspicion[url])){
+
+                var count = Math.abs(api_url_suspicion[url]);
+
+
+                if(count >= cutoff) {
+                  $(thing).animate({backgroundColor:colorFromSuspicion(count)},1000);
+                }
+
+
+                // If the user has suspected this post, dont add suspect link.
+
+                if(api_url_suspicion[url] > 0)
+                  flat_list.append("<li><a href='' class='suspectpost' value='"+url+"'>mark ad</a></li>");
+
+              } else {
+                  flat_list.append("<li><a href='' class='suspectpost' value='"+url+"'>mark ad</a></li>");
+                  if(defined(hail_url_upvotes[url])){
+
+                  var votes = hail_url_upvotes[url][0];
+
+
+                  // And if it matches one on r/hailcorporate,
+                  // Prepend "(ad)" to the title and set the post background accordingly.
+                  // (more upvoted on r/hailcorporate -> darker background)
+                  title.prepend('<a href="'+hail_url_upvotes[url][1]+'">(ad) </a>');
+                  $(thing).animate({backgroundColor:colorFromVotes(votes)},1000);
+                  }
+              }
             }
-          });
+
+            if(defined(username)) {
+              username = username.toLowerCase();
+              var username_line = $(thing).find(".entry .tagline a.author");
+
+
+              if (defined(api_username_suspicion[username])) {
+                var count = api_username_suspicion[username];
+
+                if(Math.abs(api_username_suspicion[username]) >= cutoff) {
+                  title.prepend('(shill) ');
+                  $(thing).animate({backgroundColor:colorFromSuspicion(Math.abs(count))},1000);
+                }
+
+
+                // Negative count means user has already suspected shill. Add unsuspect link.
+                if (count < 0)
+                  username_line.after(" (<a style='color:#888' href='' class='unsuspectuser' value='"+username+"'>unmark shill</a>) ");
+                if (count >= 0)
+                  username_line.after(" (<a style='color:#888' href='' class='suspectuser' value='"+username+"'>mark shill</a>) ");
+
+            } else {
+              username_line.after(" (<a style='color:#888' href='' class='suspectuser' value='"+username+"'>mark shill</a>) ");
+            }
+          }
+
+
+  });
+
+
 }
 
 
-// Fetch the urls from the last week on r/hailcorporate and match them to the current page.
+// Fetch the urls from the last week on r/hailcorporate and match them to the current page. Also fetch the stored info from the backend.
 $.getJSON(
         "https://www.reddit.com/r/HailCorporate/top/.json?sort=top&t=week&limit=500",
         function foo(data)
@@ -57,8 +180,42 @@ $.getJSON(
             }
           );
 
-          // 2. Modify posts.
-          modifyPosts();
+
+          // Grab all the current pages post-urls and poster-usernames.
+          var page_urls = [];
+          var page_usernames = [];
+          $( ".thing" ).each(function(i, thing ) {
+
+                    var url = $(thing).find(".entry .flat-list li.first a").attr('href');
+                    var username = $(thing).find(".entry .tagline a.author").text();
+                    if(url !== undefined && url != "")
+                      page_urls.push(url.toLowerCase());
+                    if(username !== undefined && username != "")
+                      page_usernames.push(username.toLowerCase());
+          });
+
+          // Match these with backend
+          var packet = {
+            accuser_username: current_user,
+            page_urls: page_urls,
+            page_usernames: page_usernames
+          };
+
+          $.ajax({
+            type: 'POST',
+            url: 'https://yogatime.herokuapp.com/api/hail_fetch',
+            dataType: 'json',
+            data: packet,
+            success: function(data) {
+              api_url_suspicion = data["suspected_urls"];
+              api_username_suspicion = data["suspected_users"];
+            }
+          })
+          .always(function() {
+              modifyPosts();
+            });
+
+
 
 
 
